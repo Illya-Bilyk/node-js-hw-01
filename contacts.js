2774da95b9cc289c1218e6e0998f98ee7b62163a
Illya-Bilyk <bilikillya.i@gmail.com>
2023-10-05 21:21:13 +0300

    hw-01

---
+++ contacts.js
@@ -0,0 +1,56 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "./db", "contacts.json");
+
+async function listContacts() {
+  const allContacts = await fs.readFile(contactsPath);
+
+  return JSON.parse(allContacts);
+}
+
+async function getContactById(contactId) {
+  const allContacts = await listContacts();
+  const contact = allContacts.find((item) => item.id === contactId);
+
+  return contact ? contact : null;
+}
+
+async function removeContact(contactId) {
+  const allContacts = await listContacts();
+
+  const contactIndex = allContacts.findIndex((item) => item.id === contactId);
+
+  if (contactIndex !== -1) {
+    const deletedContact = allContacts.splice(contactIndex, 1);
+
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+
+    return deletedContact;
+  }
+  return null;
+}
+
+async function addContact({ name, email, phone }) {
+  const newContact = {
+    id: nanoid(),
+    name: name,
+    email: email,
+    phone: phone,
+  };
+
+  const allContacts = await listContacts();
+  allContacts.push(newContact);
+
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
